b3c0ae091afff47553ed64e3874b7badae03a576
shatha <shathaobiedat62@gmail.com>
2026-09-21 15:19:38 +0300

    task1

---
+++ mon_21_9_2026/js-task1/script-task1.js
@@ -0,0 +1,20 @@
+console.log(typeof(100)); //number
+console.log(typeof(73.9));//number
+console.log(typeof(NaN)); //number
+console.log(typeof("Water"));//string
+console.log(typeof(false));//boolean
+console.log(typeof(9!=11)); //boolean
+console.log(typeof("Orange"+"e")); //string
+console.log(typeof("Orange"-"s"));//number
+console.log(typeof("4"+"8"));// string
+console.log(typeof("4"-"8"));//number
+console.log(typeof("name"+3));//string
+console.log(typeof("name"-3));//number
+console.log(typeof( 82 * "word"));//number
+console.log(typeof( 1 + "hello"));//string
+console.log(typeof( "hello" + 1));//string
+console.log(typeof(  1 + true));//number
+console.log(typeof(   "hello" + true));//string
+console.log(typeof( Infinity));//number
+console.log(typeof( 1 == '1'));//boolean
+console.log(typeof(  1 === '1'));//boolean
